97a5fc94c4a5e64308b801538be09db66847ba0b
kateMoscoso <katiemscs@gmail.com>
2020-05-15 13:08:56 +0200

    deploy

---
+++ assets/js/31.a5b21cbe.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{378:function(t,s,e){"use strict";e.r(s);var n=e(43),o=Object(n.a)({},(function(){var t=this.$createElement,s=this._self._c||t;return s("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[s("p",[this._v("https://lenguajecss.com/p/css/propiedades/flexbox")])])}),[],!1,null,null,null);s.default=o.exports}}]);
