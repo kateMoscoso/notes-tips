b13985787cc1de924bda77cbfa92e114ddde8987
kateMoscoso <katiemscs@gmail.com>
2020-05-21 12:07:05 +0200

    config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  base: '/',
+  locales: {
+    '/' :{
+      lang : 'es-SP',
+      title: 'Hola ;)',
+      description: 'Web con mis apuntes'
+    },
+    '/en/':{
+      lang : 'en-US',
+      title: 'Hello ;)',
+      description: 'Just playing around'
+    }
+  },
+  themeConfig: {
+    locales: {
+      '/' :{
+        sidebar: [
+          ['/language-framework/javascript/', 'Javascript'],
+          ['/language-framework/vue/', 'Vue'],
+          ['/docker/', 'Docker']
+        ]
+      }
+    }
+  },
+  markdown: {
+    plugins: [
+      '@org/foo', // equals to @org/markdown-it-foo if exists
+      ['markdown-it-bar', {
+        // provide options here
+      }]
+    ]
+  }
+}
+
